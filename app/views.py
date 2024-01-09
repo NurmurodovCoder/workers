a904cea55125de2267f20f11e0da2c2d42d8a121
NurmurodovCoder <nurmurodovruslan2004@gmail.com>
2024-01-09 10:19:48 +0500

    2-commit

--- app/views.py
+++ app/views.py
@@ -1,14 +1,18 @@
 from .models import Person
 from .serializers import PersonSerializers
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .permissions import PersonDetailPermission
 
 
 class PersonCreateListAPIView(generics.ListCreateAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializers
+    permission_classes = IsAuthenticated
 
 
 class PersonRetrieveAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializers
+    permission_classes = PersonDetailPermission
 
